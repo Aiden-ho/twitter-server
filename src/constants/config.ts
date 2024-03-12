5eb7aca5ca24a27227d48874eb2a5ce31d660157
Ho Tuan Kiet <aiden.hokiet@gmail.com>
2024-03-12 23:09:45 +0700

    config file mapping with env file , change name file .env,  get env file from commad npm

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,4 +1,64 @@
 import argv from 'minimist'
+import { config } from 'dotenv'
+import path from 'path'
+import { existsSync } from 'fs'
 
-const option = argv(process.argv.slice(2))
-export const isProduction = Boolean(option.production)
+const env = process.env.NODE_ENV
+const envFilename = `.env.${env}`
+
+if (!env) {
+  console.log(`Bạn chưa cung cấp biến môi trường NODE_ENV (ví dụ: development, production)`)
+  console.log(`Phát hiện NODE_ENV = ${env}`)
+  process.exit(1)
+}
+console.log(`Phát hiện NODE_ENV = ${env}, vì thế app sẽ dùng file môi trường là ${envFilename}`)
+if (!existsSync(path.resolve(envFilename))) {
+  console.log(`Không tìm thấy file môi trường ${envFilename}`)
+  console.log(`Lưu ý: App không dùng file .env, ví dụ môi trường là development thì app sẽ dùng file .env.development`)
+  console.log(`Vui lòng tạo file ${envFilename} và tham khảo nội dung ở file .env.example`)
+  process.exit(1)
+}
+
+config({
+  path: envFilename
+})
+
+export const isProduction = Boolean(env === 'production')
+
+export const envConfig = {
+  port: (process.env.PORT as string) || 4000,
+  host: process.env.HOST as string,
+  clientUrl: process.env.CLIENT_URL as string,
+  dbUsername: process.env.DB_USERNAME as string,
+  dbPassword: process.env.DB_PASSWORD as string,
+  dbName: process.env.DB_NAME as string,
+  dbUserCollection: process.env.DB_USERS_COLLECTION as string,
+  dbRefreshTokenCollection: process.env.DB_REFRESH_TOKEN_COLLECTION as string,
+  dbFollowersCollection: process.env.DB_FOLLOWERS_COLLECTION as string,
+  dbVideosStatusCollection: process.env.DB_VIDEOS_STATUS_COLLECTION as string,
+  dbTweetsCollection: process.env.DB_TWEETS_COLLECTION as string,
+  dbHashtagsCollection: process.env.DB_HASHTAGS_COLLECTION as string,
+  dbBookmarksCollection: process.env.DB_BOOKMARKS_COLLECTION as string,
+  dbLikesCollection: process.env.DB_LIKES_COLLECTION as string,
+  dbConversationsCollection: process.env.DB_CONVERSATIONS_COLLECTION as string,
+  maximumLimitTweet: process.env.MAXIMUM_LIMIT_TWEET as string,
+  miniumLimitTweet: process.env.MINIMUM_LIMIT_TWEET as string,
+  passwordSecret: process.env.PASSWORD_SECRET as string,
+  jwtSecretAccessToken: process.env.JWT_SECRET_ACCESS_TOKEN as string,
+  jwtSecretRefreshToken: process.env.JWT_SECRET_REFRESH_TOKEN as string,
+  jwtSecretEmailVerifyToken: process.env.JWT_SECRET_EMAIL_VERIFY_TOKEN as string,
+  jwtSecretForgotPasswordToken: process.env.JWT_SECRET_FORGOT_PASSWORD_TOKEN as string,
+  accessTokenExpired: process.env.ACCESS_TOKEN_EXPIRED as string,
+  refreshTokenExpired: process.env.REFRESH_TOKEN_EXPIRED as string,
+  emailVerifyTokenExpired: process.env.EMAIL_VERIFY_TOKEN_EXPIRED as string,
+  forgotPasswordTokenExpired: process.env.FORGOT_PASSWORD_TOKEN_EXPIRED as string,
+  googleClientId: process.env.GOOGLE_CLIENT_ID as string,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+  googleRedirectUri: process.env.GOOGLE_REDIRECT_URI as string,
+  clientRedirectCallback: process.env.CLIENT_REDIRECT_CALLBACK as string,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+  awsSecretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+  awsRegion: process.env.AWS_REGION as string,
+  sesFromAddress: process.env.SES_FROM_ADDRESS as string,
+  awsS3Bucket: process.env.AWS_S3_BUCKET as string
+}
